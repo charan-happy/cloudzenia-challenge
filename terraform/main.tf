53edbfce26d7f48a8e3f85c30ddc1a26a3294989
charan-happy <nagacharan4286@gmail.com>
2025-04-20 22:52:26 +0000

    updated code for ecs-setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,39 +1,46 @@
 provider "aws" {
-     region = "ap-south-1"
-   }
+  region = "ap-south-1"
+}
 
-   module "vpc" {
-     source = "./modules/vpc"
-   }
+module "vpc" {
+  source = "./modules/vpc"
+}
 
-   module "rds" {
-     source          = "./modules/rds"
-     vpc_id          = module.vpc.vpc_id
-     private_subnets = module.vpc.private_subnets
-     ecs_sg_id       = module.ecs.ecs_sg_id
-     db_password     = var.password
-   }
+module "rds" {
+  source          = "./modules/rds"
+  vpc_id          = module.vpc.vpc_id
+  private_subnets = module.vpc.private_subnets
+  ecs_sg_id       = module.ecs.ecs_sg_id
+  db_password     = var.db_password
+}
 
-   module "secrets_manager" {
-     source      = "./modules/secrets_manager"
-     db_password = var.password
-   }
+module "secrets_manager" {
+  source      = "./modules/secrets_manager"
+  db_password = var.db_password
+}
 
-   module "ecs" {
-     source                    = "./modules/ecs"
-     vpc_id                    = module.vpc.vpc_id
-     private_subnets           = module.vpc.private_subnets
-     alb_sg_id                 = module.alb.alb_sg_id
-     rds_endpoint              = module.rds.rds_endpoint
-     db_password_secret_arn    = module.secrets_manager.db_password_secret_arn
-     wordpress_target_group_arn = module.alb.wordpress_target_group_arn
-     microservice_target_group_arn = module.alb.microservice_target_group_arn
-     ecr_repository_url        = "631478160867.dkr.ecr.ap-south-1.amazonaws.com/microservice"
-   }
+module "ecs" {
+  source                    = "./modules/ecs"
+  vpc_id                    = module.vpc.vpc_id
+  private_subnets           = module.vpc.private_subnets
+  alb_sg_id                 = module.alb.alb_sg_id
+  rds_endpoint              = module.rds.rds_endpoint
+  db_password_secret_arn    = module.secrets_manager.db_password_secret_arn
+  wordpress_target_group_arn = module.alb.wordpress_target_group_arn
+  microservice_target_group_arn = module.alb.microservice_target_group_arn
+  ecr_repository_url        = "631478160867.dkr.ecr.ap-south-1.amazonaws.com/microservice"
+}
 
-   module "alb" {
-     source          = "./modules/alb"
-     vpc_id          = module.vpc.vpc_id
-     public_subnets  = module.vpc.public_subnets
-     hosted_zone_id  = "Z05135773VBNXAZ3YSQ5W"
-   }
+module "ec2" {
+  source          = "./modules/ec2"
+  vpc_id          = module.vpc.vpc_id
+  private_subnets = module.vpc.private_subnets
+  alb_sg_id       = module.alb.alb_sg_id
+}
+
+module "alb" {
+  source          = "./modules/alb"
+  vpc_id          = module.vpc.vpc_id
+  public_subnets  = module.vpc.public_subnets
+  hosted_zone_id  = var.hosted_zone_id
+}
