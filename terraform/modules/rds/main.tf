fca44fd14cd8818cd1a3ddb3deff7c787f905993
charan-happy <nagacharan4286@gmail.com>
2025-04-22 06:06:41 +0000

    updated code for secrets management

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -4,10 +4,10 @@ resource "aws_db_instance" "main" {
      engine_version      = "8.0"
      instance_class      = "db.t4g.micro"
      allocated_storage   = 20
-     username            = "wordpress_user"
+     username            = "admin"
      password            = var.db_password
      db_name             = "wordpress"
-     vpc_security_group_ids = [aws_security_group.rds.id]
+     vpc_security_group_ids = [var.ecs_sg_id]
      db_subnet_group_name = aws_db_subnet_group.main.name
      backup_retention_period = 7
      skip_final_snapshot = true
@@ -18,18 +18,3 @@ resource "aws_db_instance" "main" {
      subnet_ids = var.private_subnets
    }
 
-   resource "aws_security_group" "rds" {
-     vpc_id = var.vpc_id
-     ingress {
-       from_port   = 3306
-       to_port     = 3306
-       protocol    = "tcp"
-       security_groups = [var.ecs_sg_id]
-     }
-     egress {
-       from_port   = 0
-       to_port     = 0
-       protocol    = "-1"
-       cidr_blocks = ["0.0.0.0/0"]
-     }
-   }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,10 @@
 variable "hosted_zone_id" {
   description = "Route 53 Hosted Zone ID for nagacharan.site"
   type        = string
-  default     = "xyz"
 }
 
 variable "db_password" {
   description = "RDS database password"
   type        = string
-  default     = "xyz@1"
+  sensitive   = true
 }
