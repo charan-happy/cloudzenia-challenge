978f7eb0e908e1d954aef19a994d2e3a4c12ca59
charan-happy <nagacharan4286@gmail.com>
2025-05-04 07:55:17 +0000

    updated variables file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,13 @@
 variable "hosted_zone_id" {
   description = "Route 53 Hosted Zone ID for nagacharan.site"
   type        = string
+  value = "Z05135773VBNXAZ3YSQ5W"
+  sensitive = true
 }
 
 variable "db_password" {
   description = "RDS database password"
   type        = string
   sensitive   = true
+  value = "Iamcharan.123"
 }
